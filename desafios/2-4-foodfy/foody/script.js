09b503042ce6ec5cf5b0f65e8141d2ab74339a4c
emanuelcontinib <emanuelbertol3@gmail.com>
2020-06-29 08:42:46 -0300

    update progress

--- desafios/2-4-foodfy/foody/script.js
+++ desafios/2-4-foodfy/foody/script.js
@@ -1,23 +1,22 @@
-const modalOverlay = document.querySelector('.modal-overlay')/*usa-se o mesmo nome que o seletor do CSS*/ 
+const modalOverlay = document.querySelector('.modal-overlay') /*usa-se o mesmo nome que o seletor do CSS*/
 const cards = document.querySelectorAll('.mais-acessadas__card')
 
-for (let card of cards){
-    card.addEventListener('click', function(e){
+for (let card of cards) {
+    card.addEventListener('click', function () {
         const fotoId = card.querySelector("img").getAttribute("alt");
-        console.log(fotoId)
         const title = card.querySelector("p.nome").textContent;
         const autor = card.querySelector("p.autor").textContent;
 
         modalOverlay.querySelector("img").src = `./assets/${fotoId}.png`;
         modalOverlay.querySelector("p.nome").innerHTML = title;
         modalOverlay.querySelector("p.autor").innerHTML = autor;
-        
         modalOverlay.classList.add('active')
-        
+        modalOverlay.querySelector('#mais-acessadas__card')
+            .src = `#mais-acessadas__card`
     })
 }
 
-document.querySelector('.close-modal').addEventListener('click', function(){
+document.querySelector('.close-modal').addEventListener('click', function () {
     modalOverlay.classList.remove('active')
-    
+
 })
