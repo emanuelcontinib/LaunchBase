4da8bac1f607955994b80c66052e5f77f711fcf0
emanuelcontinib <emanuelbertol3@gmail.com>
2020-07-01 13:38:10 -0300

    update progress

--- desafios/3-1/server.js
+++ desafios/3-1/server.js
@@ -1,16 +1,34 @@
 const express = require('express')
+const nunjucks = require("nunjucks")
 
 const server = express()
 
 server.use(express.static('public'))
 
-server.set("view engine", "html")//setando o engine usando html
+server.set("view engine", "html") //setando o engine usando html
 
-
-server.get("/", function (req, res) {
-    return res.render("2-3")  //renderizar a página index
+nunjucks.configure("views", { //configurando o nujucks
+    express: server
 })
 
-server.listen(4000, function () { //porta e depois a função//
+server.get("/home", function (req, res) {
+    return res.render("home") //renderizar a página index
+})
+
+
+server.get("/about", function (req, res) {
+    return res.render("about") //renderizar a página classes
+})
+
+server.get("/courses", function (req, res) {
+    return res.render("courses") //renderizar a página classes
+})
+
+server.use(function (req, res) {
+    res.status(404).render("not-found");
+});
+
+server.listen(5000, function () { //porta e depois a função//
     console.log("server ok")
-}) 
+})
+
