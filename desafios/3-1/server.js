cb15a825617ab28972635103daef06ceb1590794
emanuelcontinib <emanuelbertol3@gmail.com>
2020-07-06 13:45:39 -0300

    update progress

--- desafios/3-1/server.js
+++ desafios/3-1/server.js
@@ -2,13 +2,15 @@ const express = require("express")
 const nunjucks = require("nunjucks")
 
 const server = express()
+const cards = require('./data')
 
 server.use(express.static('public'))
 
 server.set("view engine", "njk") //setando o motor engine usando html
 
 nunjucks.configure("views", { //configurando o nujucks
-    express: server
+    express: server,
+    autoescape:false
 })
 
 server.get("/", function (req, res) {
@@ -16,11 +18,17 @@ server.get("/", function (req, res) {
 
 
 server.get("/about", function (req, res) {
-    return res.render("about") //renderizar a página classes
+    const about = {
+        src:`https://www.reshape.com.br/assets/uploads/rocketseat.png`,
+        h3:"Treinamento imersivo nas tecnologias mais modernas de desenvolvimento",
+        modules: "13 módulos",
+        coast: 'R$ Pago'
+    }
+    return res.render("about", {about}) //renderizar a página classes
 })
 
 server.get("/courses", function (req, res) {
-    return res.render("courses") //renderizar a página classes
+    return res.render("courses", {items:cards}) //renderizar a página classes
 })
 
 server.use(function (req, res) {
