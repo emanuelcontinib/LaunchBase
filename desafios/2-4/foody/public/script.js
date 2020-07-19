f1049615c4fe6790b439eac89061b46bf2946a40
emanuelcontinib <emanuelbertol3@gmail.com>
2020-07-19 15:24:22 -0300

    update Desafio 3-3

--- desafios/2-4/foody/public/script.js
+++ desafios/2-4/foody/public/script.js
@@ -1,3 +1,5 @@
+const modalOverlay = document.querySelector(".modal-overlay");
+const cards = document.querySelectorAll(".mais-acessadas__card");
 
 for (let card of cards) {
     card.addEventListener('click', function () {
@@ -5,11 +7,11 @@ for (let card of cards) {
         const title = card.querySelector("p.nome").textContent;
         const autor = card.querySelector("p.autor").textContent;
 
-        modalOverlay.querySelector("img").src = `./assets/${fotoId}.png`;
-        modalOverlay.querySelector("p.nome").innerHTML = title;
-        modalOverlay.querySelector("p.autor").innerHTML = autor;
-        modalOverlay.classList.add('active')
-        modalOverlay.querySelector('#mais-acessadas__card')
+        card.querySelector("img").src = `./assets/${fotoId}.png`;
+        card.querySelector("p.nome").innerHTML = title;
+        card.querySelector("p.autor").innerHTML = autor;
+        card.classList.add('active')
+        card.querySelector('#mais-acessadas__card')
             .src = `#mais-acessadas__card`
     })
 }
