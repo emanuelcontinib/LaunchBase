36e1986ed1048109d7a0bfd95052a5950e879a76
emanuelcontinib <emanuelbertol3@gmail.com>
2020-07-28 11:18:50 -0300

    update progress

---
+++ module04/server.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const nunjucks = require ("nunjucks")
+const routes = require('./routes')
+const server = express()
+
+server.use(express.static('public'))
+server.use(routes)
+server.set("view engine", "njk")//setando o engine usando html
+
+nunjucks.configure("views",{ //configurando o nujucks
+    express: server,
+    autoescape:false,
+    noCache:true    
+})
+
+
+server.listen(5000, function () { //porta e depois a função//
+    console.log("server ok")
+})  
