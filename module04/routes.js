085d62e88df8d5cf814c6ed42edab3c658526b39
emanuelcontinib <emanuelbertol3@gmail.com>
2020-08-07 17:39:48 -0300

    iniciando formulários

--- module04/routes.js
+++ module04/routes.js
@@ -4,11 +4,23 @@ const routes = express.Router()
 routes.get('/', function(req,res){
 return res.redirect("/instructors/")
 })
+
 routes.get('/instructors', function(req,res){
   return res.render("instructors/index")
 })
+
+routes.get('/instructors/create', function(req,res){
+  return res.render("instructors/create")
+})
+
+routes.post('./instructors/create', function(req,res){
+
+})
+
 routes.get('/members', function(req,res){
   return res.send("/members")
 })
 
 module.exports = routes
+
+/**/
