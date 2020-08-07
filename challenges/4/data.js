ec96218808c084b99f1378e96473759229593f21
emanuelcontinib <emanuelbertol3@gmail.com>
2020-08-07 08:00:58 -0300

    update structure

---
+++ challenges/4/data.js
@@ -0,0 +1,34 @@
+module.exports = [
+    {
+        id:"cprMYC8PCVY",
+        title:"WorkshopDev Especial Episódio 1/5",
+        description:"Rocketseat",
+        coast:"Grátis",
+        featured:true
+    },
+    {
+        id:"3XnK3UPP8fw",
+        title:"WorkshopDev Especial Episódio 2/5",
+        description:"Rocketseat",
+        coast:"Grátis"
+    },
+    {
+        id:"sF2-77fUzQo",
+        title:"WorkshopDev Especial Episódio 3/5",
+        description:"Rocketseat",
+        coast:"Grátis"
+    },
+    {
+        id:"KnBGxNHOLZQ",
+        title:"WorkshopDev Especial Episódio 4/5",
+        description:"Rocketseat",
+        coast:"Grátis"
+    },
+    {
+        id:"wkjyNfBjT74",
+        title:"WorkshopDev Especial Episódio 5/5",
+        description:"Rocketseat",
+        coast:"Grátis",
+        featured: true
+    },
+]    
