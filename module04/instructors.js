47ac792b249db21ea2f6a39d821a881200659d46
emanuelcontinib <emanuelbertol3@gmail.com>
2020-08-31 17:05:10 -0300

    manipulating data

--- module04/instructors.js
+++ module04/instructors.js
@@ -1,5 +1,26 @@
 const fs = require('fs')
 const data = require('./data.json')
+
+exports.show = function (req,res) {
+  const {id} = req.params
+
+  const foundInstructor = data.instructors.find(function(instructor){
+
+    return instructor.id == id
+  })
+  if (!foundInstructor) return res.send ("Instructor not found")
+
+  const instructor = {
+    ...foundInstructor,
+    age:"",
+    gender:"",
+    services:"",
+    created_at:""
+  }
+
+  return res.render("instructors/show", { instructor: foundInstructor})
+}
+
 //POST//
 exports.post = function (req, res) {
 
